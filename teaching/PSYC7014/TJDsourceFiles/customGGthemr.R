3b935c5d20a9f068d23595f43c8caf20a0cb597a
Tehran Davis <tehran.davis@gmail.com>
2017-10-18 16:46:24 -0400

    uploading ggthemr custom

---
+++ teaching/PSYC7014/TJDsourceFiles/customGGthemr.R
@@ -0,0 +1,15 @@
+apa <- ggthemr::define_palette(
+  swatch = c("black","grey40","grey55","grey70","grey85"),
+  text = c("black","black"),
+  line = c("black","black"),
+  background = "white",
+  gradient = c("grey20","grey90")
+)
+
+webUC <- ggthemr::define_palette(
+  swatch = c("black","red","grey55","grey70","grey85"),
+  text = c("black","black"),
+  line = c("black","black"),
+  background = "white",
+  gradient = c("grey20","grey90")
+)
