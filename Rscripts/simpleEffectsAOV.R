4c6e808aa93cd19f7831f2e84b498e8ef6910afc
Tehran Davis <tehran.davis@gmail.com>
2017-11-18 13:00:55 -0500

    updated simple effects

--- Rscripts/simpleEffectsAOV.R
+++ Rscripts/simpleEffectsAOV.R
@@ -1,4 +1,5 @@
 simpleEffectsAOV <- function(full.aov,simple.aov){
+  require()
   # get important values from full:
   full_mse <- full.aov$anova_table$MSE[1]
   full_df <- full.aov$anova_table$'den Df'[1]
@@ -17,12 +18,15 @@ simpleEffectsAOV <- function(full.aov,simple.aov){
   # get corrected P
   p.value <- (1-pf(corrected_f,df1 = simple_df,df2 = full_df))
   
-  p.value <- ifelse(p.value>.001,round(p.value,3),"<.001")
+  # round p.value to 3 digits
+  p.value <- round(p,value,3)
+  
+  p.value <- ifelse(p.value>.001,paste0("p = ", p.value), "p < .001")
   
   # get simple effects pes:
   pes <- (simple_ss/(simple_ss+full_ss_error))
   
-  output <- paste0("F(",simple_df, ",", full_df,") = ", round(corrected_f,digits = 3), ", p=", p.value, ", pes=", round(pes,3))
+  output <- paste0("F(",simple_df, ",", full_df,") = ", round(corrected_f,digits = 3), ", ", p.value, ", pes=", round(pes,3))
   
   # round our obtained full_mse to 3 digits: 
   full_mse <- round(full_mse,digits = 3)
